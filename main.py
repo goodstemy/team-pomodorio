59ff4f4c07d5c35d1fb78fde21f6c2442b176aed
Ivan Efremov <baho.39rus@gmail.com>
2019-02-23 17:43:07 +0300

    Update displaying time; Add basic pomodoro cycles; Refactor idea

--- main.py
+++ main.py
@@ -24,50 +24,75 @@ class MainWindow(GridLayout):
         self.is_started = False
         self.seconds = 0
         self.interval = None
+        self.heartbeat_interval = None
+        self.periods_count = 0
+        self.is_rest = False
 
         self.cols = 1
         self.timer_label = Label(text='00:00')
-        self.button = Button(text='Start', on_press=self.click_callback)
         self.input = TextInput(text='Connect or create a team', multiline=False, on_text_validate=self.execute_connect)
 
         self.add_widget(self.input)
         self.add_widget(self.timer_label)
-        self.add_widget(self.button)
 
-    def click_callback(self, instance):
-        if self.is_started:
-            self.is_started = not self.is_started
-            self.stop_timer()
-        else:
-            self.is_started = not self.is_started
-            self.start_timer()
-
-    def start_timer(self):
+    def start_timer(self, seconds):
         print('started')
+        self.seconds = seconds
+        if self.interval:
+            self.interval.cancel()
         self.interval = Clock.schedule_interval(self.tick, 1)
-        self.button.text = 'Stop'
+        self.is_started = True
 
     def stop_timer(self):
         print('stoped')
         self.interval.cancel()
         self.timer_label.text = '00:00' 
-        self.button.text = 'Start'
+        self.is_started = False
 
     def tick(self, dt):
         self.seconds += 1
-        if len(str(self.seconds)) == 2:
-            self.timer_label.text = "00:{}".format(self.seconds)
-        else:
-            self.timer_label.text = "00:0{}".format(self.seconds)
+
+        if self.seconds >= 1500:
+            self.seconds = 0
+            self.is_rest = True
+
+        if self.seconds >= 300 and self.is_rest == True:
+            self.seconds = 0
+            self.periods_count += 1
+            self.is_rest = False
+
+        self.display_time()
+
+    def display_time(self):
+        minutes = int(self.seconds / 60)
+        seconds = self.seconds % 60
+
+        if len(str(seconds)) == 1:
+            seconds = "0{}".format(seconds)
+
+        if len(str(minutes)) == 1:
+            minutes = "0{}".format(minutes)
+
+        self.timer_label.text = "{}:{}".format(minutes, seconds)
 
     def execute_connect(self, instance):
         params = urllib.parse.urlencode({'name': instance.text})
         headers = {'Content-type': 'application/x-www-form-urlencoded', 'Accept': 'text/plain'}
-        req = UrlRequest(TEAM_POMODORO_URI, on_success=self.successed_request, req_body=params, req_headers=headers)
+        req = UrlRequest(TEAM_POMODORO_URI, on_success=self.successed_request, on_failure=self.failure_request, req_body=params, req_headers=headers)
 
     def successed_request(self, req, result):
         time = json.loads(result)
-        print(time['seconds'])
+        seconds = int(time['seconds'])
+        if seconds > 0:
+            self.start_timer(seconds)
+            print('you are connected')
+        else:
+            self.start_timer(0)
+            print('you are create new room')
+
+
+    def failure_request(self, req, error):
+        print(error)
 
 class PomodoroApp(App):
     def build(self):
